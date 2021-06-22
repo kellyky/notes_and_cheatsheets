cab5eaa5dfadce663b730a3672846b1e4303c5f2
kellyky <kelpopko@gmail.com>
2021-06-22 10:36:35 -0500

    Update file name

---
+++ http_request_methods.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "GET"
+echo "ONLY retrieves information for the request resource of the given URI"
+
+echo ""
+echo ""
+
+echo "POST"
+echo "Send data to the server to *create* a new resource."
+
+echo ""
+echo ""
+
+
+echo "PUT"
+echo "Replaces *all* of the representation of target resource with the request data"
+
+echo ""
+echo ""
+
+echo "PATCH"
+echo "Partially modifies the representation of the target resource with the request data"
+
+
+echo ""
+echo ""
+
+echo "DELETE"
+echo "Removes all of the representation of the resource specified by the URO"
+
+
+echo ""
+echo ""
+
+echo "OPTIONS"
+echo "Sends the communication options for the requested resource"
